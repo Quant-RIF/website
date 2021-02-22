a4c64909da525e39ce5a609b21f5ecf5bdb2f9d4
Paunczki <johnnymikos@gmail.com>
2021-02-22 17:54:05 -0600

    Some changes, look over to see how to change site up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import quant_logo from './Quant\ Logo-128.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Quant Site</h1>
+      <h2>From: The Quant Team</h2>
+      <img src={quant_logo} alt="our-logo" />
     </div>
   );
 }
+
+/*
+This was in app div previously
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+*/
 
 export default App;
